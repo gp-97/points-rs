7178ccda249fba2a3e3a3faf03a143415c99fa34
Gaurav Patel <GauravPatel97@outlook.com>
2021-03-14 23:25:39 +0530

    Added slope calculation for 2D system

--- src/point2d.rs
+++ src/point2d.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add, Mul, Sub};
+use std::cmp::PartialEq;
+use std::ops::{Add, Div, Mul, Sub};
 
 pub struct Point2D<T> {
     x: T,
@@ -21,6 +22,18 @@ impl<T> Point2D<T> {
     {
         (self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y)
     }
+    pub fn slope(&self, other: &Point2D<T>) -> Result<T, String>
+    where
+        T: Copy + Sub + Div + Sub<Output = T> + Div<Output = T> + PartialEq,
+    {
+        let del_y = self.y - other.y;
+        let del_x = self.x - other.x;
+        if self.y == other.y {
+            Ok(del_y / del_x)
+        } else {
+            Err("Slope = Inf".to_string())
+        }
+    }
 }
 
 #[cfg(test)]
@@ -36,5 +49,16 @@ mod tests {
         let p = Point2D::<f32>::new(1.0, 2.0);
         let q = Point2D::<f32>::new(2.0, 3.0);
         assert_eq!(p.eucli_dist_sqr(&q), 2.0);
+    }
+    #[test]
+    fn slope_test() {
+        let p = Point2D::<f32>::new(0.0, 0.0);
+        let q = Point2D::<f32>::new(0.0, 0.0);
+        let slope = match p.slope(&q) {
+            Ok(res) => res,
+            Err(e) => panic!("{}", e),
+        };
+        println!("{}", slope);
+        // assert_eq!(p.slope(&q), 1.0);
     }
 }
